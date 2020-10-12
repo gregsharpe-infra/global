188af39812c83a42a1cae1e1c8459616d2c4fa7a
Greg Sharpe <git+me@gregsharpe.co.uk>
2020-10-12 18:11:58 +0100

    adding outpust

--- output.tf
+++ output.tf
@@ -5,3 +5,7 @@ output gregsharpe_acm_cert_arn {
 output gregsharpe_route53_zone_id {
   value = aws_route53_zone.root_gregsharpe.zone_id
 }
+
+output gregsharpe_route53_name_servers {
+  value = aws_route53_zone.root_gregsharpe.name_servers
+}
