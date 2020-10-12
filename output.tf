289b8552c92522ebc0d4f450f5c4568492dbf55d
Greg Sharpe <git+me@gregsharpe.co.uk>
2020-10-12 18:09:08 +0100

    Adding route53 gregsharpe.co.uk to global

--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
 output gregsharpe_acm_cert_arn {
   value = aws_acm_certificate.root_gregsharpe.arn
 }
+
+output gregsharpe_route53_zone_id {
+  value = aws_route53_zone.root_gregsharpe.zone_id
+}
---
+++ route53.tf
@@ -0,0 +1,3 @@
+resource aws_route53_zone root_gregsharpe {
+  name = "gregsharpe.co.uk"
+}
